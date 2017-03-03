e2f03f8e15b9846feb8e8c163ed97b892c21f247
Natalia <agarkowa@gmail.com>
2017-03-03 14:03:57 +0100

    Update push_test.js

--- push_test.js
+++ push_test.js
@@ -1,22 +1,4 @@
-/* global require describe it */
-
-var assert = require("assert");
-
-describe("Pushing", function() {
-  it("generates array", function() {
-    var arr = [];
-      arr[0] = 1;
-          arr.push(arr[0]);
-    for (var i = 1; i <= 10; i++) {
-      // BEGIN (write your solution here)
-           arr[i] = arr[0] + i;
-            arr.push(arr[i]);
-         }
-alert (arr);
-  
-      // END
-    }
-
-    assert.deepEqual(arr, [1,2,3,4,5,6,7,8,9,10]);
-  });
-});
+/* function makeSortArray */
+function makeArrayConsecutive2(statues) {
+ return statues.sort(function compareNumeric(a,b) {return b-a;}); 
+}
